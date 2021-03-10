2ae12b2a6f1e649df6c24d371b8443966ea1b572
Shiva Krishna, Merla <smerla@nvidia.com>
2021-03-10 12:50:34 -0800

    Address review comments to fix checking for modules loaded or not
    Signed-off-by: Shiva Krishna, Merla <smerla@nvidia.com>

--- src/toolkit.sh
+++ src/toolkit.sh
@@ -106,17 +106,8 @@ toolkit::setup::runtime_binary() {
 	cat <<- EOF | tr -s ' \t' > ${destination}/nvidia-container-runtime
 		#! /bin/sh
 
-		assert_module_loaded() {
-			local module="\${1}"
-			cat /proc/modules | grep -e "^\${module} "
-			if [ "\${?}" -eq "0" ]; then
-				return 0
-			fi
-			return 1
-		}
-
-		assert_module_loaded nvidia
-		if [ "\${?}" -ne "0" ]; then
+		cat /proc/modules | grep -e "^nvidia "
+		if [ "\${?}" != "0" ]; then
 			echo "nvidia driver modules are not yet loaded, invoking runc directly"
 			exec runc "\$@"
 		fi
